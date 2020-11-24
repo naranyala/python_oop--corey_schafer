41c4a87767d6201957ba0f1c62b404debbc0dcad
naranyala <fudzer.mh@gmail.com>
2020-11-24 18:59:06 +0700

    updaate

--- 01_classes_and_instances.py
+++ 01_classes_and_instances.py
@@ -1,10 +1,16 @@
 #print ('Hello World')
 class Employee:
    #pass
-def __init__(self):
+def __init__(self, first, last, pay):
+   self.first=first
+   self.last=last
+   self.pay=pay
+   self.email=first+'.'+last+'@company.com'
 
-emp_1=Employee()
-emp_2=Employee()
+#emp_1=Employee()
+#emp_2=Employee()
+emp_1=Employee('Corey','Schafer',50000)
+emp_2=Employee('Test','User',60000)
 
 print(emp_1)
 print(emp_2)
