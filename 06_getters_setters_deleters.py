8e0c8865339059301ca56e3bc26dd5af7c824514
naranyala <fudzer.mh@gmail.com>
2020-11-25 23:58:49 +0700

    (+) menambahkan file 06_setter_getter_deleter.py

---
+++ 06_getters_setters_deleters.py
@@ -0,0 +1,45 @@
+#Python OOP
+class Employee:
+   num_of_emps=0
+   #raise_amount=1.04
+   raise_amt=1.04
+
+   def __init__(self, first, last):
+      self.first=first
+      self.last=last
+      #self.pay=pay
+      #self.email=first+'.'+last+'@email.com'
+
+   @property
+   def email(self):
+      return '{}.{}@email.com'.format(self.first, self.last)
+
+   @property
+   def fullname(self):
+      return '{} {}'.format(self.first, self.last)
+
+   @fullname.setter
+   def fullname(self, name):
+      first, last = name.split(' ')
+      self.first=first
+      self.last=last
+
+   @fullname.deleter
+   def fullname(self):
+      print('Delete Name!')
+      self.first=None
+      self.last=None
+
+
+emp_1=Employee('John','Smith')
+
+#emp_1.first='Jim'
+emp_1.fullname='Corey Schafer'
+
+print(emp_1.first)
+print(emp_1.email)
+#print(emp_1.email())
+#print(emp_1.fullname())
+print(emp_1.fullname)
+
+del emp_1.fullname
